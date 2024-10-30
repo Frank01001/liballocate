79c31f5b6e8f7c365ec2c4f7945a2171668cdfd1
Francesco Panebianco <xfrank01001@gmail.com>
2024-10-30 14:16:30 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+#
+# This file is part of libdebug Python library (https://github.com/libdebug/libdebug).
+# Copyright (c) 2023-2024 Francesco Panebianco. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for details.
+#
+
+from setuptools import find_packages, setup
+
+setup(
+    name="liballocate",
+    version="0.1.0",
+    author="Frank01001",
+    description="A libdebug extension for the dynamic allocator",
+    packages=find_packages(include=["liballocate", "liballocate.*"]),
+    install_requires=["libdebug", "pwntools"],
+)

c8cd42794c8a67465c561375f7659b744e1ed7b9
Frank01001 <xfrank01001@gmail.com>
2024-10-30 21:53:01 +0100

    rights: copyright refactor

--- liballocate/allocators/allocator.py
+++ liballocate/allocators/allocator.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2023-2024 Francesco Panebianco. All rights reserved.
+# Copyright (c) 2024 Francesco Panebianco. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for details.
 #
 
